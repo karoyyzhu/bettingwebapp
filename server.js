4b1f493e138294a52e6ef869d1081293178b929d
Karo Zhu <karozhu@gmail.com>
2025-01-11 01:38:11 -0800

    Submit button sends bet data to JSON file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const path = require('path')
-const express = require('express')
-const ejs = require('ejs')
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const fs = require('fs')
 
-var app = express()
+var app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', function(req, res){
   res.send('Hello World');
@@ -15,3 +18,21 @@ app.get('*', function(req, res){
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+app.post('/submit-bet', (req, res) => {
+  var bet_obj = {
+    bet_amount: req.body.amount,
+    dice_number: req.body.dicenum
+  };
+
+  var json = JSON.stringify(bet_obj);
+  fs.writeFile('bet_data.json', json, 'utf-8',
+    (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Successful file write\n");
+        console.log("Contents of file:");
+        console.log(fs.readFileSync("bet_data.json", "utf8"));
+      }
+    });
+})
